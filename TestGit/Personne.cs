22c72a6946438f2b075a47cc51c356e424b67763
b.villars <bernard.villars@2isa.org>
2018-11-08 16:22:58 +0100

    Adresse

--- TestGit/Personne.cs
+++ TestGit/Personne.cs
@@ -14,9 +14,10 @@ namespace TestGit
         public string Prenom { get; set; }
         public int Age { get; set; }
         public string Qualite { get; set; }
+         public string Adresse { get; set; }
 
         //Constructeur par défaut
-        public Personne() : this("Default","Jacques", 0, "Routier") { }
+        public Personne() : this("Default","Jacques", 0, "Routier","12 rue") { }
 
         public Personne(string nom,string prenom, int age,string qualite)
          {
@@ -27,7 +28,7 @@ namespace TestGit
 
         public override string ToString()
         {
-            return String.Format("Prénom={0},Nom={1}, Age={2}, Qualité={3}", Prenom, Nom, Age, Qualite);
+            return String.Format("Prénom={0},Nom={1}, Age={2}, Qualité={3}, Adresse={3}", Prenom, Nom, Age, Qualite);
         }
     }
 
